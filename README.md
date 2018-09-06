71ff7357be5a5c1d1f29c0b1f3ff8d142690f69d
Mario-F <github@fritschen.net>
2018-09-06 16:02:32 +0200

    Added travis ci

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Database Workload Simulator
+# Database Workload Simulator [![Build Status](https://travis-ci.org/Mario-F/db-load-sim.svg?branch=master)](https://travis-ci.org/Mario-F/db-load-sim)
 Simulating distributed realistic Workload for Database
 
 ## Development
--- test/units/generators/data/text.js
+++ test/units/generators/data/text.js
@@ -12,3 +12,12 @@ it('TEXT - Simple Generator', () => {
   expect(res.get()).to.be.an('string')
   expect(res.get().split(' ').length).to.be.within(3, 6)
 })
+
+// Create 200 Text within 6,12 and check TODO
+it('TEXT - Masscreate Generator', () => {
+  const res = _.range(300).map(_ => new Text(6,12))
+  res.forEach((v) => {
+    expect(v.get()).to.be.an('string')
+    expect(v.get().split(' ').length).to.be.within(6, 12)
+  })
+})
