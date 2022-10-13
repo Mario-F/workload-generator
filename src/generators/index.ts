fae713cdb5d2733c1474533b2044971cbe65926c
Mario-F <github@fritschen.net>
2022-10-13 17:41:02 +0200

    feat: export all generators
    Signed-off-by: Mario-F <github@fritschen.net>

---
+++ src/generators/index.ts
@@ -0,0 +1,7 @@
+import data from './data';
+import suppliers from './suppliers';
+
+export default {
+  data,
+  suppliers,
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import generators from './generators'
+
+export default {
+  generators,
+}
